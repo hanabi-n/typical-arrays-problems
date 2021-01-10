60f137dc48da0709077e25004409c1af7b164a3f
hanabi <maligazdarovanargiz@gmail.com>
2021-01-10 12:57:29 +0600

    feat: add solve of task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  array = (typeof array !== 'undefined') ? array : 0;
+
+  if(array == 0){
+      return 0;
+  }
+  
+  return Math.min.apply(null, array);
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  array = (typeof array !== 'undefined') ? array : 0;
+
+  if(array == 0){
+      return 0;
+  }
+
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  array = (typeof array !== 'undefined') ? array : 0;
+
+  var tempt = 0;
+  for(var i = 0; i < array.length; i++){
+      tempt += array[i];
+  }
+
+  if(array == 0){
+      return 0;
+  }
+  
+  return (tempt/array.length);
 }
